cfe54b79fbcd9e287c4801c4f499a35736143eca
Richard Nelis de Souza <souza.richard33@hotmail.com>
2024-03-16 19:07:25 -0300

    Adcionado comandos de publicação e build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// /api/my/user
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "Health OK!" });
+});
+
 app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
